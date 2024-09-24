978fb4eda6f19ca1cd986ad3fa604370a84cbe48
blackrockxi37 <agentsmith226@gmail.com>
2024-09-24 18:49:38 +0300

    [empty]

--- teleflow.py
+++ teleflow.py
@@ -1,18 +1,19 @@
 import os
 import telebot
 
-class Bot():
+class Bot:
     def __init__(self):
-        self.token = os.environ.get('TOKEN')
-        self.user_id = os.environ.get('USER_ID')
+        self.token = os.environ.get('TG_TOKEN')
+        self.user_id = int(os.environ.get('TG_USER_ID'))
         self.bot = telebot.TeleBot(self.token)
+
         
     def send_graph(self, image):
         self.bot.send_photo(self.user_id, image)
 
     def send_text(self, text):
         self.bot.send_message(self.user_id, text)
-
+ 
     def send_file(self, file):
         self.bot.send_document(self.user_id, file)
 
